83d6153ac1948d585baf7426181d517a5d39c75a
Haein Kim <snowman8775@gmail.com>
2022-05-25 00:12:31 +0900

    이메일, 구글 로그인 구현

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,116 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity: AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    var GOOGLE_LOGIN_CODE = 9001
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+
+        auth = Firebase.auth
+
+        val emailEditText = findViewById<EditText>(R.id.login_id)
+        val passwordEditText = findViewById<EditText>(R.id.login_password)
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        finish()
+                    } else {
+                        Toast.makeText(this, "로그인 실패.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        val signupButton = findViewById<Button>(R.id.signup_button)
+        signupButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(this, "회원가입 성공.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "회원가입 실패.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        val googleSignupbtn = findViewById<SignInButton>(R.id.google_signin_btn)
+        googleSignupbtn.setOnClickListener{
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.firebase_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data!!)
+            // result가 성공했을 때 이 값을 firebase에 넘겨주기
+            if (result!!.isSuccess) {
+                var account = result.signInAccount
+                // Second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful) {
+                    // Login, 아이디와 패스워드가 맞았을 때
+//                    Toast.makeText(this,  "success", Toast.LENGTH_LONG).show()
+                    moveMainPage(task.result?.user)
+                } else {
+                    // Show the error message, 아이디와 패스워드가 틀렸을 때
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {
+        // 파이어베이스 유저 상태가 있을 경우 다음 페이지로 넘어갈 수 있음
+        if(user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Adapter
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,20 +36,14 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
 
-        /*
-                // 트리거 워닝 페이지로 이동
-        to_trigger.setOnClickListener{
-            val intent = Intent(this, SubActivityTrigger::class.java)
-            startActivity(intent)
-        }
+    override fun onStart() {
+        super.onStart()
 
-        // 비밀번호 재설정 페이지로 이동
-        to_password.setOnClickListener{
-            val intent = Intent(this, SubActivityPassword::class.java)
-            startActivity(intent)
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
-        */
     }
 
     private fun replaceFragment(fragment: Fragment) {
