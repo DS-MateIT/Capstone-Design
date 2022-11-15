07b22ab22c9ca20830822a9b86907d2a730b9ba6
SEOULminusL <leesw0901@duksung.ac.kr>
2022-11-16 05:42:28 +0900

    xml 디테일 수정

--- app/src/main/java/com/example/myapplication/RetrofitClient.kt
+++ app/src/main/java/com/example/myapplication/RetrofitClient.kt
@@ -29,8 +29,10 @@ object RetrofitClient { //singleton - 서버 호출이 필요할 때마다 객�
             var gson = GsonBuilder().setLenient().create()
 
             instance = Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:5000/") //에뮬레이터
-                //.baseUrl("http://172.31.58.130:5000//")  // 서버 돌릴때마다 주소 맞는지 확인해야함
+                //.baseUrl("http://10.0.2.2:5000/") //에뮬레이터
+                //.baseUrl("http://172.20.15.41:5000")  // 서버 돌릴때마다 주소 맞는지 확인해야함
+                //.baseUrl("http://172.31.58.130:5000")
+                .baseUrl("http://192.168.123.107:5000/")
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(client) // Retrofit 객체에 OkHttpClient 적용
--- app/src/main/java/com/example/myapplication/SubActivityTrigger.kt
+++ app/src/main/java/com/example/myapplication/SubActivityTrigger.kt
@@ -1,11 +1,27 @@
 package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.setting_trigger_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+import java.lang.String
 
 class SubActivityTrigger : AppCompatActivity(){
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var googleSignInClient: GoogleSignInClient
+    var GOOGLE_LOGIN_CODE = 9001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,9 +33,9 @@ class SubActivityTrigger : AppCompatActivity(){
         }
 
         // 추가하기 버튼
-        btn_add.setOnClickListener {
-            val intent = Intent(this, SubActivityTrigger_add::class.java)
-            startActivity(intent)
-        }
+       // btn_add.setOnClickListener {
+      //      val intent = Intent(this, SubActivityTrigger_add::class.java)
+      //      startActivity(intent)
+      //  }
     }
 }
